12bf9eb5d387dbf5e577c0d6cd5e4fed316dd59f
sidiAbdalla <sidabdallard@gmail.com>
2023-10-04 21:43:11 +0100

    Wed Oct  4 09:43:11 PM +01 2023

--- src/main/java/ma/enset/KafkaSpringCloudStream/service/PageEventService.java
+++ src/main/java/ma/enset/KafkaSpringCloudStream/service/PageEventService.java
@@ -1,5 +1,9 @@
 package ma.enset.KafkaSpringCloudStream.service;
 import ma.enset.KafkaSpringCloudStream.entity.PageEvent;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
@@ -7,6 +11,7 @@ import java.util.Date;
 import java.util.Random;
 import java.util.UUID;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Service
@@ -23,11 +28,31 @@ public class PageEventService {
     @Bean
     public Supplier<PageEvent> pageEventSupplier(){
 
-        return () -> new PageEvent(null,UUID.randomUUID().toString().substring(0,10).replaceAll("[^a-z]",""),
+        return () -> new PageEvent(null, Math.random()>0.5 ? "Page1": "Page2",
                 Math.random()>0.5 ? "User1" : "User2",
                 new Date((long) (Math.random()*System.currentTimeMillis())),
                 new Random().nextInt(9001)
         );
+    }
+    @Bean
+    public Function<PageEvent,PageEvent> pageEventFunction(){
+        return (input) -> {
+          input.setDate(new Date());
+          return input;
+        };
+    }
+    /**
+     * Kafka Stream
+     */
+    @Bean
+    public Function<KStream<String,PageEvent>,KStream<String,Long>> kStreamFunction(){
+        return (input)->{
+            return input
+                    .filter((s, pageEvent) -> pageEvent.getDuration()>100)
+                    .map((s, pageEvent) -> new KeyValue<>(pageEvent.getName(),0L))
+                    .groupBy((s, aLong) -> s, Grouped.with(Serdes.String(),Serdes.Long()))
+                    .count().toStream();
+        };
     }
 }
 
