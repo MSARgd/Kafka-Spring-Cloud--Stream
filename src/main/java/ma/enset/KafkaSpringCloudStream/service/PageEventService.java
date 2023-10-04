9fa50764fb581d21c39694d08ef673dc7cc3472e
sidiAbdalla <sidabdallard@gmail.com>
2023-10-04 11:12:25 +0100

    Wed Oct  4 11:12:25 AM +01 2023

--- src/main/java/ma/enset/KafkaSpringCloudStream/service/PageEventService.java
+++ src/main/java/ma/enset/KafkaSpringCloudStream/service/PageEventService.java
@@ -2,7 +2,13 @@ package ma.enset.KafkaSpringCloudStream.service;
 import ma.enset.KafkaSpringCloudStream.entity.PageEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Random;
+import java.util.UUID;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 @Service
 public class PageEventService {
     @Bean
@@ -13,6 +19,15 @@ public class PageEventService {
             System.out.println(input.toString());
             System.out.println("********************************");
         };
+    }
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier(){
+
+        return () -> new PageEvent(null,UUID.randomUUID().toString().substring(0,10).replaceAll("[^a-z]",""),
+                Math.random()>0.5 ? "User1" : "User2",
+                new Date((long) (Math.random()*System.currentTimeMillis())),
+                new Random().nextInt(9001)
+        );
     }
 }
 
